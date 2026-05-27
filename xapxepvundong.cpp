5d30c22f80cc326f48cbc66873d5b5d1fb70d5ac
phuong_tran <tdthang204@gmail.com>
2026-05-27 17:20:10 +0700

    Update xapxepvundong.cpp

--- xapxepvundong.cpp
+++ xapxepvundong.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 //ham tao mang
 void inramang(int a[], int n){
-	for( i = 1; i <= n; i++)
+	for( i = 0; i <= n; i++){
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+//ham vun dong
+void vundong(int a[], int n, int i){
+	int nutcha = i ;
+	int trai = 2*i+1;
+	int phai = 2*i+2;
+	
 }
