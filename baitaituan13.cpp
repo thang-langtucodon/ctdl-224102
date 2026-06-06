001b8355338514e4aba752fbd052b3a11a070548
phuong_tran <tdthang204@gmail.com>
2026-06-06 10:04:40 +0700

    Update baitaituan13.cpp

--- baitaituan13.cpp
+++ baitaituan13.cpp
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+// Định nghĩa cấu trúc của một nút (Node) trên cây
+struct Node {
+    int namSinh;
+    struct Node* left;
+    struct Node* right;
+};
+// Hàm tiện ích để tạo một nút mới
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->namSinh = value;
