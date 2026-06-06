17e0dd5211358b75e56180db3fd46f12cc296520
phuong_tran <tdthang204@gmail.com>
2026-06-06 10:12:37 +0700

    Update baitaituan13.cpp

--- baitaituan13.cpp
+++ baitaituan13.cpp
@@ -21,3 +21,22 @@ struct Node* insert(struct Node* root, int value) {
     if (root == NULL) {
         return createNode(value);
     }
+     // So sánh giá trị để phân nhánh
+    if (value <= root->namSinh) {
+        // Nếu giá trị nhỏ hơn hoặc bằng nút hiện tại -> Sang TRÁI
+        root->left = insert(root->left, value);
+    } else {
+        // Nếu giá trị lớn hơn nút hiện tại -> Sang PHẢI
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+// ham tim gia tri va in tien trinh tung buoc
+bool searchBST(struct Node* root, int value, int step) {
+    // Trường hợp gốc: Nhánh rỗng (không tìm thấy)
+    if (root == NULL) {
+        printf("-> Buoc %d: Gap cay trong (Khong tim thay %d)\n", step, value);
+        return false;
+    }
+
+    printf("-> Buoc %d: So sanh voi nut [%d]", step, root->namSinh);
